16d9c7cc116b778b86ab365a20d570dde4db57bd
zile028@gmail.com <zile028@gmail.com>
2021-03-09 22:08:22 +0100

    include partials

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include "partials/header.php" ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MusicApp</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+<h1>HOME</h1>
 
-<body>
-
-</body>
-
-</html>
+<?php include "partials/footer.php" ?>
---
+++ partials/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <a href="index.php" class="logo"><span>Music</span> App</a>
+    <p>&copy; 2021. All right reserved</p>
+
+    <a href=""><i class="fab fa-facebook-square"></i></a>
+    <a href=""><i class="fab fa-instagram"></i></a>
+    <a href=""><i class="fab fa-soundcloud"></i></a>
+    <a href=""><i class="fab fa-youtube"></i></a>
+
+</footer>
+</body>
+
+</html>
